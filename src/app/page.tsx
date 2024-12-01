2e5b56a7e1d106852b46127c161a9d4ee5a39b15
Khaydarov Mirzo <149902818+20841@users.noreply.github.com>
2024-12-01 18:53:06 +0300

    online shop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
     <Hero />
     <section className="flex flex-col space-y-12">
     <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center">
-  URGUT MAHSULOTLARI
+  URGUT SHOP
 </h1>
 
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
